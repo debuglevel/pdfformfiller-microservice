04e0b93aebda51acf74e54488a4acd2fa2b98c28
Marc Kohaupt <debuglevel@gmail.com>
2019-09-14 12:41:30 +0200

    Documentation for merge()

--- src/main/kotlin/de/debuglevel/pdfformfiller/merge/Merger.kt
+++ src/main/kotlin/de/debuglevel/pdfformfiller/merge/Merger.kt
@@ -4,6 +4,12 @@ import java.io.InputStream
 import java.io.OutputStream
 
 interface Merger {
+    /**
+     * @param pdf the PDF form to be filled
+     * @param data the data to be filled in the forms (where the key is the control name and the value is the value to fill in)
+     * @param resultPdf the modified PDF with the data filled in
+     * @param flatten true if the form controls should be removed (i.e. the PDF becomes read only)
+     */
     fun merge(
         pdf: InputStream,
         data: Map<String, String>,
