548c2ac364701b4959a2b6cc89f1cbf09a7b47e9
Marc Kohaupt <debuglevel@gmail.com>
2019-09-14 23:36:57 +0200

    Fix bug in samples

--- example/merge/post-with-embedded-form.sh
+++ example/merge/post-with-embedded-form.sh
@@ -1,1 +1,1 @@
-curl -X POST -d @upload.json -H "Content-Type: application/json" -H "Accept: application/json" http://localhost:8080/merges/
+curl -X POST -d @upload-embedded-form.json -H "Content-Type: application/json" -H "Accept: application/json" http://localhost:8080/merges/
