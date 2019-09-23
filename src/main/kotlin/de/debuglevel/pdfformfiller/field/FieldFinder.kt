20198154f47cfb3c7f3898586670e1fcaeb2ef68
Marc Kohaupt <marc.kohaupt@avm-d.de>
2019-09-23 13:56:54 +0200

    Sort fields

--- src/main/kotlin/de/debuglevel/pdfformfiller/field/FieldFinder.kt
+++ src/main/kotlin/de/debuglevel/pdfformfiller/field/FieldFinder.kt
@@ -9,14 +9,14 @@ import javax.inject.Singleton
 class FieldFinder {
     private val logger = KotlinLogging.logger {}
 
-    fun getFields(pdf: InputStream): Set<String> {
+    fun getFields(pdf: InputStream): List<String> {
         logger.debug { "Getting fields in PDF..." }
 
         val pdfReader = PdfReader(pdf)
-        val fieldKeys = pdfReader.acroFields.allFields.keys
+        val fieldKeys = pdfReader.acroFields.allFields.keys.sortedBy { it }
         pdfReader.close()
 
         logger.debug { "Got fields in PDF: $fieldKeys" }
-        return fieldKeys.toSet()
+        return fieldKeys
     }
 }
--- src/main/kotlin/de/debuglevel/pdfformfiller/field/FieldResponse.kt
+++ src/main/kotlin/de/debuglevel/pdfformfiller/field/FieldResponse.kt
@@ -1,5 +1,5 @@
 package de.debuglevel.pdfformfiller.field
 
 data class FieldResponse(
-    val fields: Set<String>
+    val fields: List<String>
 )
