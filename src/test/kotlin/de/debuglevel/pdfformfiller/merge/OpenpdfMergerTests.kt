703557fcb582ed43b86d8d58a214bdbde3ace4c9
Marc Kohaupt <marc.kohaupt@avm-d.de>
2020-08-28 11:23:42 +0200

    Test merging

---
+++ src/test/kotlin/de/debuglevel/pdfformfiller/merge/OpenpdfMergerTests.kt
@@ -0,0 +1,45 @@
+package de.debuglevel.pdfformfiller.merge
+
+import com.lowagie.text.pdf.PdfReader
+import com.lowagie.text.pdf.parser.PdfTextExtractor
+import de.debuglevel.pdfformfiller.field.TestDataProvider
+import io.micronaut.test.annotation.MicronautTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.io.ByteArrayOutputStream
+import javax.inject.Inject
+
+@MicronautTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class OpenpdfMergerTests {
+    @Inject
+    lateinit var service: OpenpdfMerger
+
+    @ParameterizedTest
+    @MethodSource("formProvider")
+    fun `fill values into form`(testForm: TestDataProvider.TestForm) {
+        // Arrange
+        val inputStream = testForm.pdfPath.toFile().inputStream()
+        val values = testForm.fields.associateBy({ it }, { "$it Value" })
+        val outputStream = ByteArrayOutputStream()
+        val flatten = true
+
+        // Act
+        service.merge(inputStream, values, outputStream, flatten)
+
+        // Assert
+        val bytes = outputStream.toByteArray()
+        val text = PdfTextExtractor(PdfReader(bytes)).getTextFromPage(1)
+        // Assertions.assertThat(text).contains(values.values) // TODO: use this line when https://github.com/LibrePDF/OpenPDF/issues/119 is fixed
+        Assertions.assertThat(text).contains(values.values.map {
+            it.replace(
+                " ",
+                "  "
+            )
+        }) // TODO: PdfTextExtractor adds two spaces between words instead of one; this line is a workaround to match this behaviour.
+    }
+
+    fun formProvider() = TestDataProvider.formProvider()
+}
